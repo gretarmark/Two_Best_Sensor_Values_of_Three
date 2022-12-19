28c75dbd8a39c0a8e67e0790f8aa5675cfe88c1f
Grétar Már Kjartansson <gretarmark@gmail.com>
2022-12-19 09:33:04 +0000

    Update BestOf3.m
    Made the code look better

--- BestOf3.m
+++ BestOf3.m
@@ -6,18 +6,18 @@ function [OUT_VAL,q_xSensor1Active,q_xSensor2Active,q_xSensor3Active] =...
     %rHaedaskynjariTemp = [0 0 0];
     
     if ~SENSOR_A_ERROR
-	    nrSensFunc = nrSensFunc + 1;
+	nrSensFunc = nrSensFunc + 1;
         rHaedaskynjariTemp(nrSensFunc) = SENSOR_A;
     end
 
     if ~SENSOR_B_ERROR
-	    nrSensFunc = nrSensFunc + 1;
-	    rHaedaskynjariTemp(nrSensFunc) = SENSOR_B;
+	nrSensFunc = nrSensFunc + 1;
+	rHaedaskynjariTemp(nrSensFunc) = SENSOR_B;
     end
 
     if ~SENSOR_C_ERROR
         nrSensFunc = nrSensFunc + 1;
-	    rHaedaskynjariTemp(nrSensFunc) = SENSOR_C;
+	rHaedaskynjariTemp(nrSensFunc) = SENSOR_C;
     end
 
 
@@ -25,12 +25,12 @@ function [OUT_VAL,q_xSensor1Active,q_xSensor2Active,q_xSensor3Active] =...
 
     if iState == 1
         OUT_VAL = rHaedaskynjariTemp(1);
-	    q_xSensor1Active = ~ SENSOR_A_ERROR;            
+	q_xSensor1Active = ~ SENSOR_A_ERROR;            
         q_xSensor2Active = ~ SENSOR_B_ERROR;            
         q_xSensor3Active = ~ SENSOR_C_ERROR;
     elseif iState == 2
         OUT_VAL = rHaedaskynjariTemp(2);
-	    q_xSensor1Active = ~SENSOR_A_ERROR;            
+	q_xSensor1Active = ~SENSOR_A_ERROR;            
         q_xSensor2Active = ~SENSOR_B_ERROR;            
         q_xSensor3Active = ~SENSOR_C_ERROR;
     elseif iState == 3
@@ -56,38 +56,38 @@ function [OUT_VAL,q_xSensor1Active,q_xSensor2Active,q_xSensor3Active] =...
 	        diff3 = abs(rHaedaskynjariTemp(2) - rHaedaskynjariTemp(3));
         
 %             if diff1 == diff2
-% 		        OUT_VAL = (rHaedaskynjariTemp(1) + rHaedaskynjariTemp(2))/2;
-% 		        q_xSensor1Active = true;            
+% 		  OUT_VAL = (rHaedaskynjariTemp(1) + rHaedaskynjariTemp(2))/2;
+% 		  q_xSensor1Active = true;            
 %                 q_xSensor2Active = true;            
 %                 q_xSensor3Active = false;
 %             if diff1 == diff3
-% 		        OUT_VAL = (rHaedaskynjariTemp(1) + rHaedaskynjariTemp(2))/2;
-% 		        q_xSensor1Active = true;            
+% 		  OUT_VAL = (rHaedaskynjariTemp(1) + rHaedaskynjariTemp(2))/2;
+% 		  q_xSensor1Active = true;            
 %                 q_xSensor2Active = true;            
 %                 q_xSensor3Active = false;
 %             elseif diff2 == diff3
-% 		        OUT_VAL = (rHaedaskynjariTemp(2) + rHaedaskynjariTemp(3))/2;
-% 		        q_xSensor1Active = false;            
+% 		  OUT_VAL = (rHaedaskynjariTemp(2) + rHaedaskynjariTemp(3))/2;
+% 		  q_xSensor1Active = false;            
 %                 q_xSensor2Active = true;            
 %                 q_xSensor3Active = true;
 %             if diff1 == diff2 && diff2 == diff3
-% 		        OUT_VAL = rHaedaskynjariTemp(1);
-% 		        q_xSensor1Active = true;            
+% 		  OUT_VAL = rHaedaskynjariTemp(1);
+% 		  q_xSensor1Active = true;            
 %                 q_xSensor2Active = false;            
 %                 q_xSensor3Active = false;
             if diff1 < diff2 && diff1 < diff3
-		        OUT_VAL = (rHaedaskynjariTemp(1) + rHaedaskynjariTemp(2))/2;
-		        q_xSensor1Active = true;            
+		OUT_VAL = (rHaedaskynjariTemp(1) + rHaedaskynjariTemp(2))/2;
+		q_xSensor1Active = true;            
                 q_xSensor2Active = true;            
                 q_xSensor3Active = false;
             elseif diff2 < diff1 && diff2 < diff3
-		        OUT_VAL = (rHaedaskynjariTemp(1) + rHaedaskynjariTemp(3))/2;
-		        q_xSensor1Active = true;            
+		OUT_VAL = (rHaedaskynjariTemp(1) + rHaedaskynjariTemp(3))/2;
+		q_xSensor1Active = true;            
                 q_xSensor2Active = false;            
                 q_xSensor3Active = true;
-	        else
-		        OUT_VAL = (rHaedaskynjariTemp(2) + rHaedaskynjariTemp(3))/2;
-		        q_xSensor1Active = false;            
+	    else
+		OUT_VAL = (rHaedaskynjariTemp(2) + rHaedaskynjariTemp(3))/2;
+		q_xSensor1Active = false;            
                 q_xSensor2Active = true;            
                 q_xSensor3Active = true;
             end
